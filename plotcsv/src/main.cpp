813cb0cba3d079c0ea49e1512698cb611d9663e0
ramonapariciog <moncho_apa@hotmail.com>
2018-08-31 16:55:05 -0500

    resolution 1024x768

--- plotcsv/src/main.cpp
+++ plotcsv/src/main.cpp
@@ -5,8 +5,8 @@
 int main(){
 
 	//ofSetupOpenGL(1920, 1080, OF_GAME_MODE); // <-------- setup the GL context
-        //ofSetupOpenGL(1024, 768, OF_GAME_MODE);
-	ofSetupOpenGL(1366, 768, OF_GAME_MODE);
+        ofSetupOpenGL(1024, 768, OF_GAME_MODE);
+	//ofSetupOpenGL(1366, 768, OF_GAME_MODE);
         // this kicks off the running of my app
 	// can be OF_WINDOW or OF_FULLSCREEN
 	// pass in width and height too:
